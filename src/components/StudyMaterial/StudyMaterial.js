efe40c841b70ebe4976db3062134af00456f6989
Mohit <2021csb1109@iitrpr.ac.in>
2023-06-15 12:48:43 +0530

    theme change

--- src/components/StudyMaterial/StudyMaterial.js
+++ src/components/StudyMaterial/StudyMaterial.js
@@ -1,11 +1,10 @@
 import React from "react";
-import Design from "../Design";
+
 const StudyMaterial=()=>
 {
     return(
         <section id='StudyMaterial' className="section">
-            <Design/>
-        
+             
         <h1>StudyMaterial</h1>
         <h1>StudyMaterial</h1>
         <h1>StudyMaterial</h1>
