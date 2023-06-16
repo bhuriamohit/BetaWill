700c2ea6e7cbb424616122f3b3b57da2b5e3f909
Kartik150704 <108309065+Kartik150704@users.noreply.github.com>
2023-06-16 10:58:17 +0530

    my courses

---
+++ src/components/Lectures/LecturePage.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { useState } from "react";
+import { useLocation } from "react-router-dom";
+
+
+const LecturePage=()=>
+{
+    return(
+        <div>
+            <h2>Lecture Page</h2>
+        </div>
+    )
+}
+export default LecturePage;
