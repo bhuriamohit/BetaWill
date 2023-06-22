8964f31b13c28fdd608a4a57c04d71fa835001ab
Kartik150704 <108309065+Kartik150704@users.noreply.github.com>
2023-06-22 21:22:51 +0530

    changes

--- src/components/UserInfo/UserInfo.js
+++ src/components/UserInfo/UserInfo.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { useLocation, useNavigate } from 'react-router-dom';
+import { useLocation, useNavigate } from "react-router-dom";
 import Mycourses from "./Mycourses";
-import './Userinfo.css';
+import "./Userinfo.css";
 
 const Userinfo = () => {
   const navigate = useNavigate();
@@ -11,13 +11,17 @@ const Userinfo = () => {
     useremail = location.state.useremail;
   }
 
-  const [fullname, setfullname] = useState("Fetching Data.......");
-  const [gender, setgender] = useState("Fetching Data.........");
-  const [dob, setdob] = useState("Fetching Data........");
-  const [country, setcountry] = useState("Fetching Data......");
-  const [state, setstate] = useState("Fetching Data.......");
-  const [pincode, setpincode] = useState("Fetching Data..........");
+  const [userinfo, setUserinfo] = useState({
+    fullname: "Fetching Data.......",
+    gender: "Fetching Data.........",
+    dob: "Fetching Data........",
+    country: "Fetching Data......",
+    state: "Fetching Data.......",
+    pincode: "Fetching Data..........",
+  });
   const [showProfile, setShowProfile] = useState(false);
+  const [editedField, setEditedField] = useState("");
+  const [editedValue, setEditedValue] = useState("");
 
   useEffect(() => {
     fetchData();
@@ -25,25 +29,56 @@ const Userinfo = () => {
 
   const fetchData = async () => {
     if (location.state == null) {
-      navigate('/');
+      navigate("/");
     } else {
       let s = "https://betawill-com.onrender.com/getinfo/" + useremail;
       let response = await fetch(s);
       let fres = await response.json();
 
       console.log(fres.userinfo);
-      setfullname(fres.userinfo.fullname);
-      setdob(fres.userinfo.dob);
-      setgender(fres.userinfo.gender);
-      setcountry(fres.userinfo.country);
-      fres = fres.userinfo;
-      setstate(fres.state);
-      setpincode(fres.pincode);
+      setUserinfo(fres.userinfo);
     }
   };
 
   const toggleProfile = () => {
     setShowProfile(!showProfile);
+    setEditedField("");
+    setEditedValue("");
+  };
+
+  const handleEdit = (field, value) => {
+    setEditedField(field);
+    setEditedValue(value);
+  };
+  
+  const handleChange =async  () => {
+    // Perform the necessary actions with editedField and editedValue
+    // For example, update the corresponding state variable with the new value
+    setUserinfo((prevUserinfo) => ({
+      ...prevUserinfo,
+      [editedField]: editedValue,
+    }));
+    let formData={
+      useremail:useremail,
+      field:editedField,
+      value:editedValue
+    }
+    fetch('http://localhost:8080/changefield', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(formData)
+    })
+    .then(async (response)=>
+    {
+        response=await response.json();
+        response=response.status;
+        alert(response)
+    })
+    
+    setEditedField("");
+    setEditedValue("");
   };
 
   return (
@@ -61,53 +96,34 @@ const Userinfo = () => {
             <span className="label">Email:</span>
             <span className="value">{useremail}</span>
           </div>
-          <div className="info">
-            <span className="label">Full Name:</span>
-            <span className="value">
-              {fullname}
-              <button className="useredit">Edit</button>
-            </span>
-          </div>
-          <div className="info">
-            <span className="label">Date of Birth:</span>
-            <span className="value">
-              {dob}
-              <button className="useredit">Edit</button>
-            </span>
-          </div>
-          <div className="info">
-            <span className="label">Gender:</span>
-            <span className="value">
-              {gender}
-              <button className="useredit">Edit</button>
-            </span>
-          </div>
-          <div className="info">
-            <span className="label">Country:</span>
-            <span className="value">
-              {country}
-              <button className="useredit">Edit</button>
-            </span>
-          </div>
-          <div className="info">
-            <span className="label">State:</span>
-            <span className="value">
-              {state}
-              <button className="useredit">Edit</button>
-            </span>
-          </div>
-          <div className="info">
-            <span className="label">Pincode:</span>
-            <span className="value">{pincode}
-            <button className="useredit">Edit</button></span>
-            
-          </div>
+          {Object.entries(userinfo).map(([field, value]) => (
+            <div className="info" key={field}>
+              <span className="label">{field[0].toUpperCase() + field.slice(1)}:</span>
+              {editedField === field ? (
+                <div className="edit-container">
+                  <input
+                    type="text"
+                    value={editedValue}
+                    onChange={(e) => setEditedValue(e.target.value)}
+                  />
+                  <button className="useredit" onClick={handleChange}>
+                    Done
+                  </button>
+                </div>
+              ) : (
+                <span className="value">
+                  {value}
+                  <button className="useredit" onClick={() => handleEdit(field, value)}>
+                    Edit
+                  </button>
+                </span>
+              )}
+            </div>
+          ))}
         </div>
       )}
 
-      <section>
-        {useremail != null && <Mycourses email={useremail} />}
-      </section>
+      <section>{useremail != null && <Mycourses email={useremail} />}</section>
     </div>
   );
 };
